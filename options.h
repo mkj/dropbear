1444bad02b96a604fbc30e826aea497068784563
Matt Johnston <matt@ucc.asn.au>
2003-03-23 17:20:41 +0000

    rel 0.255 for ltc 0.82

--- options.h
+++ options.h
@@ -67,7 +67,7 @@
  * You shouldn't edit below here unless you know you need to.
  *******************************************************************/
 
-#define DROPBEAR_VERSION "0.25"
+#define DROPBEAR_VERSION "0.255"
 #define LOCAL_IDENT "SSH-2.0-dropbear_" DROPBEAR_VERSION
 
 /* Time to wait before sending reply on fail */
