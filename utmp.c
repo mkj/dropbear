ff8ec75c3d44d8f6a08117bda9d76313f0e3cf76
Matt Johnston <matt@ucc.asn.au>
2003-05-02 10:39:04 +0000

    fixing bugs, not in use though

--- utmp.c
+++ utmp.c
@@ -5,8 +5,8 @@
 #include "chansession.h"
 #include "session.h"
 
-static char * minusslashdev(unsigned char * tty);
-static char * getttyid(char * dest, int destlen, unsigned char * tty);
+static const char * minusslashdev(const unsigned char * tty);
+static char * getttyid(char * dest, int destlen, const unsigned char * tty);
 
 /* login code, for the various types */
 
@@ -21,22 +21,20 @@ static void login_addutmp(struct ChanSess * chansess) {
 	/* we don't need to null terminate entries */
 	/* username */
 	strncpy(ut.ut_name, ses.authstate.username, sizeof(ut.ut_name));
-	dropbear_log(0, "name is %s\n", ut.ut_name);
+	dropbear_log(LOG_DEBUG, "name is %s\n", ut.ut_name);
 
-	/* terminal device minus the "/dev/" */
-	strncpy(ut.ut_line, minusslashdev(chansess->tty), sizeof(ut.ut_line));
-	dropbear_log(0, "line is %s\n", ut.ut_line);
+	strncpy(ut.ut_line, chansess->tty, sizeof(ut.ut_line));
 
 #ifdef UTMP_HAS_HOST
 	/* remote host if required*/
 	getaddrhostname(ut.ut_host, sizeof(ut.ut_host), ses.addrstring);
-	dropbear_log(0, "host is %s\n", ut.ut_host);
+	dropbear_log(LOG_DEBUG, "host is %s\n", ut.ut_host);
 #endif
 
 #ifdef UTMP_HAS_PID
 	/* pid of the process */
-	ut.ut_pid = chansess->pid;
-	dropbear_log(0, "pid is %d\n", ut.ut_pid);
+	ut.ut_pid = getpid();
+	dropbear_log(LOG_DEBUG, "pid is %d\n", ut.ut_pid);
 #endif
 
 #ifdef UTMP_HAS_TIME
@@ -56,13 +54,15 @@ static void login_addutmp(struct ChanSess * chansess) {
 #ifdef UTMP_HAS_ID
 	/* mmm, more unixy ugliness */
 	getttyid(ut.ut_id, sizeof(ut.ut_id), chansess->tty);
-	dropbear_log(0, "ut_id is %s\n", ut.ut_id);
+	dropbear_log(LOG_DEBUG, "ut_id is %.4s\n", ut.ut_id);
 #endif
 
 	/* now the parts specific to logging in */
 	ut.ut_type = USER_PROCESS;
 	
 	login(&ut);
+
+	dropbear_log(LOG_DEBUG, "line is '%s'", ut.ut_line);
 }
 #endif
 
@@ -98,34 +98,58 @@ void dropbear_addlogin(struct ChanSess * chansess) {
 }
 
 /* return a string minus the "/dev/" */
-static char * minusslashdev(unsigned char * tty) {
+static const char * minusslashdev(const unsigned char * tty) {
 
 	int len;
 
+	dropbear_log(LOG_DEBUG, "before len");
 	len = strlen(tty);
+	dropbear_log(LOG_DEBUG, "len  %d", len);
+
 	
 	/* we need to have "/dev/" at the start */
-	if ((len <= 5) || (strncmp(tty, "/dev/", 5) != 0)) {
+	if ((len <= 5) || (memcmp(tty, "/dev/", 5) != 0)) {
+		dropbear_log(LOG_DEBUG, "tty1  %s", tty);
 		return tty;
 	}
 
-	return (char*)tty+5;
+	dropbear_log(LOG_DEBUG, "tty2  %s", tty+5);
+	return tty+5;
 }
 
 #ifdef UTMP_HAS_ID
-static char * getttyid(char * dest, int destlen, unsigned char * tty) {
+/* Get the ut_id. First the "/dev/" is removed, and "tty" as well if it exists.
+ * The last destlen bytes of the remainder is the result.
+ * The result is truncated to dest, it is only null-terminated if there is
+ * space at the end. A pointer to dest is returned */
+static char * getttyid(char * dest, int destlen, const unsigned char * tty) {
 
 	int len;
 	
+	dropbear_log(LOG_DEBUG, "blah");
 	tty = minusslashdev(tty);
+	dropbear_log(LOG_DEBUG, "blah s %s", tty);
 	len = strlen(tty);
 
 	/* if it's of the form "/dev/ttya0", we want the "a0" */
-	if (len > 3) {
+	if (len > 3 && (strncmp(tty, "tty", 3) == 0)) {
 		tty += 3;
+		len -= 3;
 	}
 
-	return tty;
+	/* the last destlen bit of tty */
+	if (len > destlen) {
+		dropbear_log(LOG_DEBUG, "len - destlen = %d\n", len - destlen);
+		tty = (tty + len - destlen);
+	}
+	len = MIN(destlen, len);
+	dropbear_log(LOG_DEBUG, "len is %d\n", len);
+
+	memcpy(dest, tty, len);
+	if (len < destlen) {
+		dest[len] = '\0'; /* null terminate if there's space */
+	}
+	return dest;
 }
 #endif
 
@@ -133,9 +157,9 @@ static char * getttyid(char * dest, int destlen, unsigned char * tty) {
 void logout_delutmp(struct ChanSess *chansess) {
 
 		dropbear_log(LOG_WARNING, "doing logout(%s)", 
-				minusslashdev(chansess->tty));
-	if (logout(minusslashdev(chansess->tty)) != 0) {
-		dropbear_log(LOG_WARNING, "error with logout()");
+				(chansess->tty));
+	if (logout((chansess->tty)) != 0) {
+		dropbear_log(LOG_WARNING, "error with logout(): %s", strerror(errno));
 	}
 
 }
