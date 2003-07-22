8c1b19eea09a7dd1b7d9ede6a0f579de9b713e33
Matt Johnston <matt@ucc.asn.au>
2003-07-22 05:31:28 +0000

    update

--- tcpfwd.c
+++ tcpfwd.c
@@ -16,3 +16,61 @@ int newdirecttcp(struct Channel * chan) {
 	 * source origport */
 
 }
+
+/* Initiate a new TCP connection - this is non-blocking, so the socket
+ * returned will need to be checked for success when it is first written.
+ * Similarities with OpenSSH's connect_to() are not coincidental.
+ * Returns -1 on failure */
+static int newtcp(const char * host, int port, int origport) {
+
+	int sock;
+	char portstring[6];
+	struct addrinfo *res = NULL, *ai;
+
+	struct addrinfo hints;
+
+	TRACE(("enter newtcp"));
+
+	memset(hints, 0, sizeof(hints));
+	/* TCP, either ip4 or ip6 */
+	hints.ai_type = SOCK_STREAM;
+	hints.ai_family = PF_UNSPEC;
+
+	snprintf(portstring, sizeof(portstring), "%d", port);
+	if (getaddrinfo(host, portstring, &hints, &res) != 0) {
+		if (res) {
+			freeaddrinfo(res);
+		}
+		TRACE(("leave newtcp: failed getaddrinfo"));
+		return -1;
+	}
+
+	ai = res;
+	
+	/* Use the first socket that works */
+	for (ai = res; ai != NULL; ai = ai->ai_next) {
+		
+		if (ai->ai_family != PF_INET && ai->ai_family != PF_INET6) {
+			continue;
+		}
+
+		sock = socket(ai->ai_family, SOCK_STREAM, 0);
+		if (sock < 0) {
+			TRACE(("TCP socket() failed"));
+			continue;
+		}
+
+		if (fcntl(sock, F_SETFL, O_NONBLOCK) < 0) {
+			TRACE(("TCP non-blocking failed"));
+			continue;
+		}
+
+		/* non-blocking, so it might return without success (EINPROGRESS) */
+		if (connect(sock, ai->ai_addr, ai->ai_addrlen) < 0 &&
+				errno != EINPROGRESS) {
+			close(sock);
+			TRACE(("TCP connect failed"));
+			continue;
+		}
+	}
+}
