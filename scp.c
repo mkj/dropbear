9a868d93990b2280691d97987c1ff83398fc02c1
Matt Johnston <matt@ucc.asn.au>
2004-01-19 14:01:26 +0000

    put in a #define for timersub, so irix can have scp easily

--- scp.c
+++ scp.c
@@ -85,6 +85,19 @@
 }
 #endif
 
+#ifndef timersub
+#define timersub(tvp, uvp, vvp)					 \
+	do {								\
+		(vvp)->tv_sec = (tvp)->tv_sec - (uvp)->tv_sec;	  \
+		(vvp)->tv_usec = (tvp)->tv_usec - (uvp)->tv_usec;   \
+		if ((vvp)->tv_usec < 0) {			   \
+			(vvp)->tv_sec--;				\
+			(vvp)->tv_usec += 1000000;		  \
+		}						   \
+	} while (/* CONSTCOND */ 0)
+#endif /* timersub */
+
+
 void bwlimit(int);
 
 /* Struct for addargs */
