b5989964979e3ee885eeb6d0758d96605b131a7b
Matt Johnston <matt@ucc.asn.au>
2006-03-04 08:22:16 +0000

    Exit with a message if the ssh protocol version is incompatible
    --HG--
    extra : convert_revision : 5d396842815593611f0d61762440145d1fc74d5a

--- common-session.c
+++ common-session.c
@@ -232,10 +232,8 @@ void session_identification() {
 		dropbear_exit("Error writing ident string");
 	}
 
-	/* We allow up to 9 lines before the actual version string, to
-	 * account for wrappers/cruft etc. According to the spec only the client
-	 * needs to handle this, but no harm in letting the server handle it too */
-	for (i = 0; i < 10; i++) {
+    /* If they send more than 50 lines, something is wrong */
+	for (i = 0; i < 50; i++) {
 		len = ident_readln(ses.sock, linebuf, sizeof(linebuf));
 
 		if (len < 0 && errno != EINTR) {
@@ -258,6 +256,12 @@ void session_identification() {
 		ses.remoteident = m_malloc(len);
 		memcpy(ses.remoteident, linebuf, len);
 	}
+
+    /* Shall assume that 2.x will be backwards compatible. */
+    if (strncmp(ses.remoteident, "SSH-2.", 6) != 0
+            && strncmp(ses.remoteident, "SSH-1.99-", 9) != 0) {
+        dropbear_exit("Incompatible remote version '%s'", ses.remoteident);
+    }
 
 	TRACE(("remoteident: %s", ses.remoteident))
 
