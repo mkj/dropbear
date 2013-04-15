b4cdfcb506dd872b871ae1fad97e474c0658c286
Matt Johnston <matt@ucc.asn.au>
2013-04-15 21:56:04 +0800

    bump version to 2013.57

--- sysoptions.h
+++ sysoptions.h
@@ -4,7 +4,7 @@
  *******************************************************************/
 
 #ifndef DROPBEAR_VERSION
-#define DROPBEAR_VERSION "2013.56"
+#define DROPBEAR_VERSION "2013.57"
 #endif
 
 #define LOCAL_IDENT "SSH-2.0-dropbear_" DROPBEAR_VERSION
