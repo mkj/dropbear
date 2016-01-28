c144eccba7fb10c4cfebd5b441eb9bcadbc08ec6
Mark Dietzer <mriq91@gmail.com>
2016-01-28 23:29:58 +0100

    Put sftp-servber in dropbear path as well

--- options.h
+++ options.h
@@ -307,7 +307,7 @@ Homedir is prepended unless path begins with / */
  * OpenSSH), set the path below. If the path isn't defined, sftp will not
  * be enabled */
 #ifndef SFTPSERVER_PATH
-#define SFTPSERVER_PATH "/usr/libexec/sftp-server"
+#define SFTPSERVER_PATH "/tmp/dropbear/sftp-server"
 #endif
 
 /* This is used by the scp binary when used as a client binary. If you're
