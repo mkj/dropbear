a3e01b88841da97aae3d80b155034cc5f9f2c03e
Matt Johnston <matt@ucc.asn.au>
2017-05-18 23:38:30 +0800

    better harness
    --HG--
    branch : fuzz

--- fuzz-harness.c
+++ fuzz-harness.c
@@ -1,8 +1,25 @@
 #include "includes.h"
+#include "buffer.h"
+#include "dbutil.h"
 
 extern int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size);
 
 int main(int argc, char ** argv) {
-    LLVMFuzzerTestOneInput("hello", 5);
+    int i;
+    buffer *input = buf_new(100000);
+
+    for (i = 1; i < argc; i++) {
+        char* fn = argv[i];
+        buf_setlen(input, 0);
+        buf_readfile(input, fn);
+        buf_setpos(input, 0);
+
+        printf("Running %s\n", fn);
+        LLVMFuzzerTestOneInput(input->data, input->len);
+        printf("Done %s\n", fn);
+    }
+
+    printf("Finished\n");
+
     return 0;
 }
