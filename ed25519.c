f8df05ec1912640e5ace27c11a9fb6e131bc16e2
Peter Szabo <pts@fazekas.hu>
2017-09-30 17:09:25 +0200

    reduced the memory usage of buf_ed25519_verify

--- ed25519.c
+++ ed25519.c
@@ -1,19 +1,19 @@
 /*
  * Dropbear - a SSH2 server
- * 
+ *
  * Copyright (c) 2002,2003 Matt Johnston
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,7 +33,7 @@
  * will be in gened25519.c, since it isn't required in the server itself.
  */
 
-#ifdef DROPBEAR_ED25519 
+#ifdef DROPBEAR_ED25519
 
 /* --- Copied from tweetnacl.c.
  *
@@ -229,12 +229,9 @@ static const u64 K[80] = {
 static int crypto_hashblocks(u8 *x,const u8 *m,u64 n) {
   u64 z[8],b[8],a[8],w[16],t;
   int i,j;
-
   FOR(i,8) z[i] = a[i] = dl64(x + 8 * i);
-
   while (n >= 128) {
     FOR(i,16) w[i] = dl64(m + 8 * i);
-
     FOR(i,80) {
       FOR(j,8) b[j] = a[j];
       t = a[7] + Sigma1(a[4]) + Ch(a[4],a[5],a[6]) + K[i] + w[i%16];
@@ -245,15 +242,11 @@ static int crypto_hashblocks(u8 *x,const u8 *m,u64 n) {
 	FOR(j,16)
 	  w[j] += w[(j+9)%16] + sigma0(w[(j+1)%16]) + sigma1(w[(j+14)%16]);
     }
-
     FOR(i,8) { a[i] += z[i]; z[i] = a[i]; }
-
     m += 128;
     n -= 128;
   }
-
   FOR(i,8) ts64(x+8*i,z[i]);
-
   return n;
 }
 
@@ -271,31 +264,25 @@ static const u8 iv[64] = {
 static int crypto_hash(u8 *out,const u8 *m,u64 n) {
   u8 h[64],x[256];
   u64 i,b = n;
-
   FOR(i,64) h[i] = iv[i];
-
   crypto_hashblocks(h,m,n);
   m += n;
   n &= 127;
   m -= n;
-
   FOR(i,256) x[i] = 0;
   FOR(i,n) x[i] = m[i];
   x[n] = 128;
-
   n = 256-128*(n<112);
   x[n-9] = b >> 61;
   ts64(x+n-8,b<<3);
   crypto_hashblocks(h,x,n);
-
   FOR(i,64) out[i] = h[i];
-
   return 0;
 }
 
 static void add(gf p[4],gf q[4]) {
   gf a,b,c,d,t,e,f,g,h;
-  
+
   Z(a, p[1], p[0]);
   Z(t, q[1], q[0]);
   M(a, a, t);
@@ -310,7 +297,6 @@ static void add(gf p[4],gf q[4]) {
   Z(f, d, c);
   A(g, d, c);
   A(h, b, a);
-
   M(p[0], e, f);
   M(p[1], h, g);
   M(p[2], g, f);
@@ -325,7 +311,7 @@ static void cswap(gf p[4],gf q[4],u8 b) {
 
 static void pack(u8 *r,gf p[4]) {
   gf tx, ty, zi;
-  inv25519(zi, p[2]); 
+  inv25519(zi, p[2]);
   M(tx, p[0], zi);
   M(ty, p[1], zi);
   pack25519(r, ty);
@@ -395,30 +381,24 @@ static int crypto_sign(u8 *sm,u64 *smlen,const u8 *m,u64 n,const u8 *sk) {
   u64 i,j;
   i64 x[64];
   gf p[4];
-
   crypto_hash(d, sk, 32);
   d[0] &= 248;
   d[31] &= 127;
   d[31] |= 64;
-
   *smlen = n+64;
   FOR(i,n) sm[64 + i] = m[i];
   FOR(i,32) sm[32 + i] = d[32 + i];
-
   crypto_hash(r, sm+32, n+32);
   reduce(r);
   scalarbase(p,r);
   pack(sm,p);
-
   FOR(i,32) sm[i+32] = sk[i+32];
   crypto_hash(h,sm,n + 64);
   reduce(h);
-
   FOR(i,64) x[i] = 0;
   FOR(i,32) x[i] = (u64) r[i];
   FOR(i,32) FOR(j,32) x[i+j] += h[i] * (u64) d[j];
   modL(sm + 32,x);
-
   return 0;
 }
 
@@ -430,62 +410,47 @@ static int unpackneg(gf r[4],const u8 p[32]) {
   M(den,num,D);
   Z(num,num,r[2]);
   A(den,r[2],den);
-
   S(den2,den);
   S(den4,den2);
   M(den6,den4,den2);
   M(t,den6,num);
   M(t,t,den);
-
   pow2523(t,t);
   M(t,t,num);
   M(t,t,den);
   M(t,t,den);
   M(r[0],t,den);
-
   S(chk,r[0]);
   M(chk,chk,den);
   if (neq25519(chk, num)) M(r[0],r[0],I);
-
   S(chk,r[0]);
   M(chk,chk,den);
   if (neq25519(chk, num)) return -1;
-
   if (par25519(r[0]) == (p[31]>>7)) Z(r[0],gf0,r[0]);
-
   M(r[3],r[0],r[1]);
   return 0;
 }
 
-static int crypto_sign_open(u8 *m,u64 *mlen,const u8 *sm,u64 n,const u8 *pk) {
+/* Modified API: removed m and mlen.
+ * Overwrites sm as a side effect.
+ */
+static int crypto_sign_open_m(u8 *sm,u64 n,const u8 *pk) {
   u64 i;
   u8 t[32],h[64];
   gf p[4],q[4];
-
-  *mlen = -1;
+  u8 s[32];
   if (n < 64) return -1;
-
   if (unpackneg(q,pk)) return -1;
-
-  FOR(i,n) m[i] = sm[i];
-  FOR(i,32) m[i+32] = pk[i];
-  crypto_hash(h,m,n);
+  FOR(i,32) s[i] = sm[i+32];
+  FOR(i,32) sm[i+32] = pk[i];
+  crypto_hash(h,sm,n);
   reduce(h);
   scalarmult(p,q,h);
-
-  scalarbase(q,sm + 32);
+  scalarbase(q,s);
   add(p,q);
   pack(t,p);
-
   n -= 64;
-  if (crypto_verify_32(sm, t)) {
-    FOR(i,n) m[i] = 0;
-    return -1;
-  }
-
-  FOR(i,n) m[i] = sm[i + 64];
-  *mlen = n;
-  return 0;
+  return crypto_verify_32(sm, t);
 }
 
 /* --- */
@@ -550,25 +515,22 @@ void buf_put_ed25519_priv_key(buffer* buf, dropbear_ed25519_key *key) {
 }
 
 #ifdef DROPBEAR_SIGNKEY_VERIFY
-/* Verify a ed25519 signature (in buf) made on data by the key given. 
+/* Verify a ed25519 signature (in buf) made on data by the key given.
  * returns DROPBEAR_SUCCESS or DROPBEAR_FAILURE */
 int buf_ed25519_verify(buffer* buf, dropbear_ed25519_key *key, buffer *data_buf) {
-	char *sm, *m;
+	char *sm;
 	int res;
 	unsigned data_size;
-	u64 mlen;  /* Will be dat_buf->len. */
 	if (buf_getint(buf) != 64 || buf->len - buf->pos != 64) {
 		return DROPBEAR_FAILURE;
 	}
 	/* Typical data_size is 148 bytes for ssh. */
 	sm = m_malloc(64 + (data_size = data_buf->len));
 	/* !! TODO(pts): Do 512 bytes on the stack (without malloc). */
-	/* !! TODO(pts): Do it without too much copying and allocation. Maybe reuse m as sm first. */
+	/* !! TODO(pts): Do it without too much copying and allocation. */
 	memcpy(sm, buf->data + buf->pos, 64);
 	memcpy(sm + 64, data_buf->data, data_size);
-	m = m_malloc(64 + data_size);
-	res = crypto_sign_open(m, &mlen, sm, data_size + 64, key->spk + 32);
-	free(m);
+	res = crypto_sign_open_m(sm, data_size + 64, key->spk + 32);
 	free(sm);
 	return res ? DROPBEAR_FAILURE : DROPBEAR_SUCCESS;
 }
