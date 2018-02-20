c154b0626beea0299f490b293a78d479959389ec
Francois Perrad <francois.perrad@gadz.org>
2018-02-20 20:39:01 +0100

    const parameter

--- svr-auth.c
+++ svr-auth.c
@@ -36,7 +36,7 @@
 #include "dbrandom.h"
 
 static void authclear(void);
-static int checkusername(char *username, unsigned int userlen);
+static int checkusername(const char *username, unsigned int userlen);
 
 /* initialise the first time for a session, resetting all parameters */
 void svr_authinitialise() {
@@ -227,7 +227,7 @@ out:
 
 /* Check that the username exists and isn't disallowed (root), and has a valid shell.
  * returns DROPBEAR_SUCCESS on valid username, DROPBEAR_FAILURE on failure */
-static int checkusername(char *username, unsigned int userlen) {
+static int checkusername(const char *username, unsigned int userlen) {
 
 	char* listshell = NULL;
 	char* usershell = NULL;
