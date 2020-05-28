07d46f9907d6d3f5d6f1ceba1bbbe41bad732760
Matt Johnston <matt@ucc.asn.au>
2020-05-28 23:23:54 +0800

    Fix typo DROPBEAR_NORMAL_DH

--- dh_groups.h
+++ dh_groups.h
@@ -2,7 +2,7 @@
 #define DROPBEAR_DH_GROUPS_H
 #include "options.h"
 
-#if DROPBEAR_DH_NORMAL
+#if DROPBEAR_NORMAL_DH
 
 #if DROPBEAR_DH_GROUP1
 #define DH_P_1_LEN 128
@@ -21,6 +21,6 @@ extern const unsigned char dh_p_16[DH_P_16_LEN];
 
 extern const int DH_G_VAL;
 
-#endif
+#endif /* DROPBEAR_NORMAL_DH */
 
 #endif
