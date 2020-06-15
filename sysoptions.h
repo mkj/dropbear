ee4b4db816a2e77fb81290b1ad0c2e6a8af3f099
Matt Johnston <matt@ucc.asn.au>
2020-06-15 23:36:14 +0800

    changelog for 2020.79

--- sysoptions.h
+++ sysoptions.h
@@ -4,7 +4,7 @@
  *******************************************************************/
 
 #ifndef DROPBEAR_VERSION
-#define DROPBEAR_VERSION "2019.78"
+#define DROPBEAR_VERSION "2020.79"
 #endif
 
 #define LOCAL_IDENT "SSH-2.0-dropbear_" DROPBEAR_VERSION
