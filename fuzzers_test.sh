1eb369272bc8a23acc52bf11f81ff39b0d04f95b
Matt Johnston <matt@ucc.asn.au>
2021-03-07 21:26:34 +0800

    fuzz: add -q quiet argument for standalone fuzzers. travis has a log length limit

--- fuzzers_test.sh
+++ fuzzers_test.sh
@@ -7,7 +7,8 @@ result=0
 test -d fuzzcorpus && hg --repository fuzzcorpus/ pull || hg clone https://hg.ucc.asn.au/dropbear-fuzzcorpus fuzzcorpus || exit 1
 for f in `make list-fuzz-targets`; do
     # use xargs to split the too-long argument list
-    echo fuzzcorpus/$f/* | xargs -n 1000 ./$f || result=1
+    # -q quiet because travis has a logfile limit
+    echo fuzzcorpus/$f/* | xargs -n 1000 ./$f -q || result=1
 done
 
 exit $result
