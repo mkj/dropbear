712d529164e0cf6bd497e337dd10684385dcb4b9
Egor Duda <egor.duda@gmail.com>
2022-01-17 18:33:24 +0300

    Keys with type sk-* make no sense as host keys, so they should be disabled

--- svr-runopts.c
+++ svr-runopts.c
@@ -668,6 +668,12 @@ void load_all_hostkeys() {
 		any_keys = 1;
 	}
 #endif
+#if DROPBEAR_SK_ECDSA
+	disablekey(DROPBEAR_SIGNKEY_SK_ECDSA);
+#endif 
+#if DROPBEAR_SK_ED25519
+	disablekey(DROPBEAR_SIGNKEY_SK_ED25519);
+#endif
 
 	if (!any_keys) {
 		dropbear_exit("No hostkeys available. 'dropbear -R' may be useful or run dropbearkey.");
