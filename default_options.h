d9035968bd4c17dcb535658376fe7325af5c1be0
HansH111 <hans@atbas.org>
2022-03-12 12:33:31 +0000

    add DEBUG_LEVEL define to add limited tracing

--- default_options.h
+++ default_options.h
@@ -48,6 +48,11 @@ IMPORTANT: Some options will require "make clean" after changes */
  * This will add a reasonable amount to your executable size. */
 #define DEBUG_TRACE 0
 
+/* Include limited verbose debug output, the number of debug messages
+ * are controlled by specifying 1 or more -v at runtime.
+ * This will add approx 2 kB to your executable size. */
+#define DEBUG_LEVEL 0
+
 /* Set this if you want to use the DROPBEAR_SMALL_CODE option. This can save
  * several kB in binary size however will make the symmetrical ciphers and hashes
  * slower, perhaps by 50%. Recommended for small systems that aren't doing
