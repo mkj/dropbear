a528245c2653541e7fec1d235a0e8d33ecaee70c
HansH111 <hans@atbas.org>
2022-03-12 16:57:42 +0000

    tracelevel for remote ident

--- common-session.c
+++ common-session.c
@@ -404,7 +404,7 @@ static void read_session_identification() {
 		dropbear_exit("Incompatible remote version '%s'", ses.remoteident);
 	}
 
-	TRACE(("remoteident: %s", ses.remoteident))
+	TRACELEVEL((1,"remoteident: %s", ses.remoteident))
 
 }
 
