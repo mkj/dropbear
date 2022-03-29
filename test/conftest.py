ee3828b3f83812b41ecad1bd97f15411abca04a8
Matt Johnston <matt@ucc.asn.au>
2022-03-29 22:29:17 +0800

    Add tests for dropbearconvert

--- test/conftest.py
+++ test/conftest.py
@@ -2,9 +2,12 @@ def pytest_addoption(parser):
     parser.addoption("--port", type=str, help="default is 2244 local, 22 remote")
     parser.addoption("--dbclient", type=str, default="../dbclient")
     parser.addoption("--dropbear", type=str, default="../dropbear")
+    parser.addoption("--dropbearconvert", type=str, default="../dropbearconvert")
+    parser.addoption("--dropbearkey", type=str, default="../dropbearkey")
     parser.addoption("--hostkey", type=str, help="required unless --remote")
     parser.addoption("--remote", type=str, help="remote host")
     parser.addoption("--user", type=str, help="optional username")
+    parser.addoption("--ssh-keygen", type=str, default="ssh-keygen")
 
 def pytest_configure(config):
     opt = config.option
