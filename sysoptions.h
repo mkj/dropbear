b8669b063bd2fc1906a13fb4b50c8b4697bf49ce
Matt Johnston <matt@ucc.asn.au>
2022-04-01 14:43:27 +0800

    Bump version to 2022.82

--- sysoptions.h
+++ sysoptions.h
@@ -4,7 +4,7 @@
  *******************************************************************/
 
 #ifndef DROPBEAR_VERSION
-#define DROPBEAR_VERSION "2020.81"
+#define DROPBEAR_VERSION "2022.82"
 #endif
 
 #define LOCAL_IDENT "SSH-2.0-dropbear_" DROPBEAR_VERSION
