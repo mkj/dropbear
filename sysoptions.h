mkj/dropbear

commit 787391ea3b5af2acf5e3c83372510f0c79477ad7

Force DROPBEAR_DSS when fuzzing

sysoptions.h
@@ -382,4 +382,10 @@
 #define DROPBEAR_MULTI 0
 #endif
 
+/* Fuzzing expects all key types to be enabled */
+#ifdef DROPBEAR_DSS
+#undef DROPBEAR_DSS
+#endif
+#define DROPBEAR_DSS 1
+
 /* no include guard for this file */

commit c043efb47c3173072fa636ca0da0d19875d4511f

Fix so DROPBEAR_DSS is only forced for fuzzing
Regression from 787391ea3b5af2acf5e3c83372510f0c79477ad7,
was missing fuzzing conditional

sysoptions.h
@@ -380,9 +380,11 @@
 #endif
 
 /* Fuzzing expects all key types to be enabled */
+#if DROPBEAR_FUZZ
 #if defined(DROPBEAR_DSS)
 #undef DROPBEAR_DSS
 #endif
 #define DROPBEAR_DSS 1
+#endif
 
 /* no include guard for this file */