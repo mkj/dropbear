c96e6c66b2eae49ca45fa5217524087a3a2e222c
Matt Johnston <matt@ucc.asn.au>
2024-01-22 09:44:30 +0800

    Use src/distrooptions.h as another source
    This makes it easier for distributions to patch the source directory,
    rather than needing to create localoptions.h in the build directory.
    
    Makefile's OPTION_HEADERS is removed, the wildcard *.h covers it

--- src/options.h
+++ src/options.h
@@ -1,17 +1,22 @@
 #ifndef DROPBEAR_OPTIONS_H
 #define DROPBEAR_OPTIONS_H
 
-/* 
-            > > > Don't edit this file any more! < < <
-            
+/*
 Local compile-time configuration should be defined in localoptions.h
-in the build directory.
+in the build directory, or src/distrooptions.h
 See default_options.h.in for a description of the available options.
 */
 
 /* Some configuration options or checks depend on system config */
 #include "config.h"
 
+/* Distribution custom build options, used if it exists */
+#ifdef DISTROOPTIONS_H_EXISTS
+#include "distrooptions.h"
+#endif
+
+/* Local compile-time configuration in the build directory,
+ * used if it exists */
 #ifdef LOCALOPTIONS_H_EXISTS
 #include "localoptions.h"
 #endif
