82f593283f7de9a0b30f7a3e6e4bc673f878b872
Matt Johnston <matt@ucc.asn.au>
2024-04-04 19:01:26 +0800

    changelog for 2024.84

--- src/sysoptions.h
+++ src/sysoptions.h
@@ -4,7 +4,7 @@
  *******************************************************************/
 
 #ifndef DROPBEAR_VERSION
-#define DROPBEAR_VERSION "2022.83"
+#define DROPBEAR_VERSION "2024.84"
 #endif
 
 #define LOCAL_IDENT "SSH-2.0-dropbear_" DROPBEAR_VERSION
