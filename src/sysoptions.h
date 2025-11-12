mkj/dropbear

commit 440b7b5c4ffb9c36c2ffc24efbf3733c296d5909

Add sntrup761x25519-sha512 post-quantum key exchange
This follows draft-ietf-sshm-ntruprime-ssh-01, using the sntrup761
implementation from supercop. It is available under both
sntrup761x25519-sha512 and sntrup761x25519-sha512@openssh.com names.

Interoperability has been tested against OpenSSH 9.8 (client/server) and
PuTTY 0.82 client.

sntrup761.sh is taken from OpenSSH, to extract the code from the
supercop distribution.

KEX hash buffer size calculation has been updated to current algorithm
limits, since sntrup761 was larger than the previous limit.

Code size increases by approx 9kB (209 to 218kB) for a 32-bit armv7 build.

src/sysoptions.h
@@ -208,8 +208,7 @@
 #define DROPBEAR_SK_ED25519 ((DROPBEAR_SK_KEYS) && (DROPBEAR_ED25519))
 #endif
 
-/* XXX: Not actually used */
-#define DROPBEAR_PQHYBRID 1
+#define DROPBEAR_PQHYBRID DROPBEAR_SNTRUP761
 #define DROPBEAR_CURVE25519_DEP (DROPBEAR_CURVE25519 || DROPBEAR_PQHYBRID)
 
 /* Dropbear only uses server-sig-algs, only needed if we have rsa-sha256 pubkey auth */
@@ -245,16 +244,39 @@
 #define MAX_STRING_LEN (MAX(MAX_CMD_LEN, 2400)) /* Sun SSH needs 2400 for algos,
                                                    MAX_CMD_LEN is usually longer */
 
-/* For a 4096 bit DSS key, empirically determined */
-#define MAX_PUBKEY_SIZE 1700
-/* For a 4096 bit DSS key, empirically determined */
+
+/* Key type sizes are ordered large to small, all are
+ determined empirically, and rounded up */
+#if DROPBEAR_RSA
+/* 4096 bit RSA key */
+#define MAX_PUBKEY_SIZE 600
 #define MAX_PRIVKEY_SIZE 1700
+#elif DROPBEAR_DSS
+#define MAX_PUBKEY_SIZE 500
+#define MAX_PRIVKEY_SIZE 500
+#else
+/* 521 bit ecdsa key */
+#define MAX_PUBKEY_SIZE 200
+#define MAX_PRIVKEY_SIZE 200
+#endif
+
+/* For kex hash buffer, worst case size for Q_C || Q_S || K */
+#if DROPBEAR_SNTRUP761
+/* 2337 */
+#define MAX_KEX_PARTS (2*4 + 1158 + 1039 + 32*2 + 68)
+#elif DROPBEAR_DH_GROUP16
+/* 4096 bit group */
+#define MAX_KEX_PARTS (3 * 520)
+#else
+/* Sufficent for 2048 bit group14, or ecdsa521 */
+#define MAX_KEX_PARTS 1000
+#endif
 
 #define MAX_HOSTKEYS 4
 
 /* The maximum size of the bignum portion of the kexhash buffer */
-/* Sect. 8 of the transport rfc 4253, K_S + e + f + K */
-#define KEXHASHBUF_MAX_INTS (1700 + 130 + 130 + 130)
+/* K_S + Q_C + Q_S + K */
+#define KEXHASHBUF_MAX_INTS (MAX_PUBKEY_SIZE + MAX_KEX_PARTS)
 
 #define DROPBEAR_MAX_SOCKS 2 /* IPv4, IPv6 are all we'll get for now. Revisit
 								in a few years time.... */

.github/workflows/build.yml
@@ -61,6 +61,9 @@ jobs:
             # enable all options
             nondefault: 1
             configure_flags: --enable-pam
+            # sntrup761.c is not c89 compliant
+            localoptions: |
+              #define DROPBEAR_SNTRUP761 0
 
           - name: macos 14
             os: macos-14
@@ -134,7 +137,7 @@ jobs:
               #define DROPBEAR_USE_PASSWORD_ENV 0
               #define DROPBEAR_SFTPSERVER 0
 
-          - name: other algo combos
+          - name: no sha1
             runcheck: 'no'
             # disables all sha1
             localoptions: |
@@ -217,7 +220,7 @@ jobs:
         if: ${{ matrix.nondefault }}
         run: |
           # Turn on anything that's off by default. Rough but seems sufficient
-          grep ' 0$' src/default_options.h | sed 's/0$/1/' > localoptions.h
+          grep ' 0$' src/default_options.h | sed 's/0$/1/' >> localoptions.h
           # PAM clashes with password
           echo "#define DROPBEAR_SVR_PASSWORD_AUTH 0" >> localoptions.h
           # 1 second timeout is too short

Makefile.in
@@ -64,7 +64,8 @@ _CLISVROBJS=common-session.o packet.o common-algo.o common-kex.o \
 		common-channel.o common-chansession.o termcodes.o loginrec.o \
 		tcp-accept.o listener.o process-packet.o dh_groups.o \
 		common-runopts.o circbuffer.o list.o netio.o chachapoly.o gcm.o \
-		kex-x25519.o kex-dh.o kex-ecdh.o kex-pqhybrid.o
+		kex-x25519.o kex-dh.o kex-ecdh.o kex-pqhybrid.o \
+		sntrup761.o
 CLISVROBJS = $(patsubst %,$(OBJ_DIR)/%,$(_CLISVROBJS))
 
 _KEYOBJS=dropbearkey.o

src/common-algo.c
@@ -33,6 +33,7 @@
 #include "gcm.h"
 #include "chachapoly.h"
 #include "ssh.h"
+#include "sntrup761.h"
 
 /* This file (algo.c) organises the ciphers which can be used, and is used to
  * decide which ciphers/hashes/compression/signing to use during key exchange*/
@@ -266,12 +267,35 @@ static const struct dropbear_kex kex_ecdh_nistp521 = {DROPBEAR_KEX_ECDH, NULL, 0
 #endif /* DROPBEAR_ECDH */
 
 #if DROPBEAR_CURVE25519
-/* Referred to directly */
 static const struct dropbear_kex kex_curve25519 = {DROPBEAR_KEX_CURVE25519, NULL, 0, NULL, &sha256_desc };
 #endif
 
+
+#if DROPBEAR_SNTRUP761
+static const struct dropbear_kem_desc sntrup761_desc = {
+	.public_len = crypto_kem_sntrup761_PUBLICKEYBYTES,
+	.secret_len = crypto_kem_sntrup761_SECRETKEYBYTES,
+	.ciphertext_len = crypto_kem_sntrup761_CIPHERTEXTBYTES,
+	.output_len = crypto_kem_sntrup761_BYTES,
+	.kem_gen = crypto_kem_sntrup761_keypair,
+	.kem_enc = crypto_kem_sntrup761_enc,
+	.kem_dec = crypto_kem_sntrup761_dec,
+};
+static const struct dropbear_kex kex_sntrup761 = {DROPBEAR_KEX_PQHYBRID, NULL, 0, &sntrup761_desc, &sha512_desc };
+#endif
+
+/* For sntrup761 */
+volatile int16_t crypto_int16_optblocker = 0;
+volatile int32_t crypto_int32_optblocker = 0;
+volatile int64_t crypto_int64_optblocker = 0;
+
+
 /* data == NULL for non-kex algorithm identifiers */
 algo_type sshkex[] = {
+#if DROPBEAR_SNTRUP761
+	{"sntrup761x25519-sha512", 0, &kex_sntrup761, 1, NULL},
+	{"sntrup761x25519-sha512@openssh.com", 0, &kex_sntrup761, 1, NULL},
+#endif
 #if DROPBEAR_CURVE25519
 	{"curve25519-sha256", 0, &kex_curve25519, 1, NULL},
 	{"curve25519-sha256@libssh.org", 0, &kex_curve25519, 1, NULL},

src/default_options.h
@@ -185,6 +185,7 @@ IMPORTANT: Some options will require "make clean" after changes */
  * group1 - 1024 bit, sha1
  * curve25519 - elliptic curve DH
  * ecdh - NIST elliptic curve DH (256, 384, 521)
+ * sntrup761 - post-quantum hybrid with x25519.
  *
  * group1 is too small for security though is necessary if you need
      compatibility with some implementations such as Dropbear versions < 0.53
@@ -194,6 +195,10 @@ IMPORTANT: Some options will require "make clean" after changes */
  * curve25519 increases binary size by ~2,5kB on x86-64
  * including either ECDH or ECDSA increases binary size by ~30kB on x86-64
 
+ * sntrup761 is recommended to avoid possible decryption
+ * by future quantum computers.
+ * It is fast, but adds ~9kB code size (32-bit armv7)
+
  * Small systems should generally include either curve25519 or ecdh for performance.
  * curve25519 is less widely supported but is faster
  */
@@ -201,6 +206,7 @@ IMPORTANT: Some options will require "make clean" after changes */
 #define DROPBEAR_DH_GROUP14_SHA256 1
 #define DROPBEAR_DH_GROUP16 0
 #define DROPBEAR_CURVE25519 1
+#define DROPBEAR_SNTRUP761 1
 #define DROPBEAR_ECDH 1
 #define DROPBEAR_DH_GROUP1 0
 

src/sntrup761.h
@@ -1,0 +1,13 @@
+#ifndef SNTRUP761_H
+#define SNTRUP761_H
+
+#define crypto_kem_sntrup761_PUBLICKEYBYTES 1158
+#define crypto_kem_sntrup761_SECRETKEYBYTES 1763
+#define crypto_kem_sntrup761_CIPHERTEXTBYTES 1039
+#define crypto_kem_sntrup761_BYTES 32
+
+int crypto_kem_sntrup761_keypair(unsigned char *pk, unsigned char *sk);
+int crypto_kem_sntrup761_enc(unsigned char *c, unsigned char *k, const unsigned char *pk);
+int crypto_kem_sntrup761_dec(unsigned char *k, const unsigned char *c, const unsigned char *sk);
+
+#endif /* SNTRUP761_H */

src/sntrup761_compat.h
@@ -1,0 +1,44 @@
+/* To be included only by sntrup761.c, provides
+ * random and sha512 implementation from Dropbear.
+ * Partially based on OpenSSH crypto_api.h */
+/*
+ * Assembled from generated headers and source files by Markus Friedl.
+ * Placed in the public domain.
+ */
+
+#include "includes.h"
+#include "dbrandom.h"
+#include "sntrup761.h"
+
+#if DROPBEAR_SNTRUP761
+
+typedef int8_t crypto_int8;
+typedef uint8_t crypto_uint8;
+typedef int16_t crypto_int16;
+typedef uint16_t crypto_uint16;
+typedef int32_t crypto_int32;
+typedef uint32_t crypto_uint32;
+typedef int64_t crypto_int64;
+typedef uint64_t crypto_uint64;
+
+static inline void randombytes(unsigned char* buf, unsigned int len) {
+    genrandom(buf, len);
+}
+
+static inline uint32_t small_random32(void) {
+    uint32_t v;
+    genrandom((unsigned char*)&v, sizeof(v));
+    return v;
+}
+
+static int crypto_hash_sha512(uint8_t *out, const uint8_t *m,
+    unsigned long long n)
+{
+  hash_state hs;
+
+  sha512_init(&hs);
+  sha512_process(&hs, m, n);
+  return sha512_done(&hs, out);
+}
+
+#endif /* DROPBEAR_SNTRUP761 */

commit 7b8e47a74398efbef02429825c0e37d835cc9eef

Fix too small ecdsa private key buffer
When RSA or DSS are not built, the private key buffer size is too small,
resulting in a "bad_getbuf" or similar exit. This was a regression in
2025.87.
Fixes: 440b7b5c4ffb ("Add sntrup761x25519-sha512 post-quantum key exchange")

ECDSA private key requires around 241 bytes
4 + len("ecdsa-sha2-nistp521") + 4 + (521/8+1 + 4)*3 + 4

Fixes #368 on github.

src/sysoptions.h
@@ -266,7 +266,7 @@
 #else
 /* 521 bit ecdsa key */
 #define MAX_PUBKEY_SIZE 200
-#define MAX_PRIVKEY_SIZE 200
+#define MAX_PRIVKEY_SIZE 250
 #endif
 
 /* For kex hash buffer, worst case size for Q_C || Q_S || K */