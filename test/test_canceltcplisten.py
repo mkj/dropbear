mkj/dropbear

commit 1984aabc950904a9717735ae82b0bbf7d89d3478

Server shouldn't return "localhost" in response to -R forward connections if that wasn't what the client requested.

svr-tcpfwd.c
@@ -172,14 +172,14 @@ out:
 static int svr_remotetcpreq() {
 
 	int ret = DROPBEAR_FAILURE;
-	unsigned char * bindaddr = NULL;
+	unsigned char * request_addr = NULL;
 	unsigned int addrlen;
 	struct TCPListener *tcpinfo = NULL;
 	unsigned int port;
 
 	TRACE(("enter remotetcpreq"))
 
-	bindaddr = buf_getstring(ses.payload, &addrlen);
+	request_addr = buf_getstring(ses.payload, &addrlen);
 	if (addrlen > MAX_IP_LEN) {
 		TRACE(("addr len too long: %d", addrlen))
 		goto out;
@@ -209,12 +209,15 @@ static int svr_remotetcpreq() {
 	tcpinfo->chantype = &svr_chan_tcpremote;
 	tcpinfo->tcp_type = forwarded;
 
-	if (!opts.listen_fwd_all || (strcmp(bindaddr, "localhost") == 0) ) {
+	tcpinfo->request_listenaddr = request_addr;
+	if (!opts.listen_fwd_all || (strcmp(request_addr, "localhost") == 0) ) {
         // NULL means "localhost only"
-		m_free(bindaddr);
-		bindaddr = NULL;
+		tcpinfo->listenaddr = NULL;
 	}
-	tcpinfo->listenaddr = bindaddr;
+	else
+	{
+		tcpinfo->listenaddr = request_addr;
+	}
 
 	ret = listen_tcpfwd(tcpinfo);
 
@@ -222,7 +225,7 @@ out:
 	if (ret == DROPBEAR_FAILURE) {
 		/* we only free it if a listener wasn't created, since the listener
 		 * has to remember it if it's to be cancelled */
-		m_free(bindaddr);
+		m_free(request_addr);
 		m_free(tcpinfo);
 	}
 	TRACE(("leave remotetcpreq"))

tcp-accept.c
@@ -40,6 +40,7 @@ static void cleanup_tcp(struct Listener *listener) {
 
 	m_free(tcpinfo->sendaddr);
 	m_free(tcpinfo->listenaddr);
+	m_free(tcpinfo->request_listenaddr);
 	m_free(tcpinfo);
 }
 
@@ -78,7 +79,7 @@ static void tcp_acceptor(struct Listener *listener, int sock) {
 			dropbear_assert(tcpinfo->tcp_type == forwarded);
 			/* "forwarded-tcpip" */
 			/* address that was connected, port that was connected */
-			addr = tcpinfo->listenaddr;
+			addr = tcpinfo->request_listenaddr;
 			port = tcpinfo->listenport;
 		}
 

tcpfwd.h
@@ -39,6 +39,8 @@ struct TCPListener {
 	 * localhost, or a normal interface name. */
 	unsigned char *listenaddr;
 	unsigned int listenport;
+	/* The address that the remote host asked to listen on */
+	unsigned char *request_listenaddr;;
 
 	const struct ChanType *chantype;
 	enum {direct, forwarded} tcp_type;

commit f5d44406ef2952ca69a68d59c6b0f7f0ff777305

Fix cancelling server TCP listeners
There are three bugs involved here.

get_listener() only tested the first element in the list rather than
iterating.  This was incorrect when first written in
444dbb536479 ("- Reworked non-channel fd handling to listener.c - More channel cleaning up")

tcpinfo.chantype isn't initialised in svr_cancelremotetcp(), so
matchtcp() never succeeded. The chantype comparison is unnecessary
(LISTENER_TYPE_TCPFORWARDED has already been checked), so remove it.

matchtcp() was comparing with listenaddr, but that will be NULL
when listening on localhost, logic in svr_remotetcpreq().
That regressed in 1984aabc9509 ("Server shouldn't return "localhost" in response to -R forward connections if that wasn't what the client requested.")
The NULL pointer previously couldn't be reached due to the chantype
problem mentioned above.

A testcase is added.

Fixes #412, fixes #413

src/listener.c
@@ -136,9 +136,9 @@ struct Listener * get_listener(enum ListenerType type, const void* typedata,
 		int (*match)(const void*, const void*)) {
 
 	unsigned int i;
-	struct Listener* listener;
 
-	for (i = 0, listener = ses.listeners[i]; i < ses.listensize; i++) {
+	for (i = 0; i < ses.listensize; i++) {
+		struct Listener* listener = ses.listeners[i];
 		if (listener && listener->type == type
 				&& match(typedata, listener->typedata)) {
 			return listener;

src/svr-tcpfwd.c
@@ -132,14 +132,13 @@ static int matchtcp(const void* typedata1, const void* typedata2) {
 	const struct TCPListener *info2 = (struct TCPListener*)typedata2;
 
 	return (info1->listenport == info2->listenport)
-			&& (info1->chantype == info2->chantype)
-			&& (strcmp(info1->listenaddr, info2->listenaddr) == 0);
+			&& (strcmp(info1->request_listenaddr, info2->request_listenaddr) == 0);
 }
 
 static int svr_cancelremotetcp() {
 
 	int ret = DROPBEAR_FAILURE;
-	char * bindaddr = NULL;
+	char * request_addr = NULL;
 	unsigned int addrlen;
 	unsigned int port;
 	struct Listener * listener = NULL;
@@ -147,7 +146,7 @@ static int svr_cancelremotetcp() {
 
 	TRACE(("enter cancelremotetcp"))
 
-	bindaddr = buf_getstring(ses.payload, &addrlen);
+	request_addr = buf_getstring(ses.payload, &addrlen);
 	if (addrlen > MAX_HOST_LEN) {
 		TRACE(("addr len too long: %d", addrlen))
 		goto out;
@@ -155,18 +154,17 @@ static int svr_cancelremotetcp() {
 
 	port = buf_getint(ses.payload);
 
-	tcpinfo.sendaddr = NULL;
-	tcpinfo.sendport = 0;
-	tcpinfo.listenaddr = bindaddr;
+	memset(&tcpinfo, 0x0, sizeof(tcpinfo));
+	tcpinfo.request_listenaddr = request_addr;
 	tcpinfo.listenport = port;
 	listener = get_listener(LISTENER_TYPE_TCPFORWARDED, &tcpinfo, matchtcp);
 	if (listener) {
-		remove_listener( listener );
+		remove_listener(listener);
 		ret = DROPBEAR_SUCCESS;
 	}
 
 out:
-	m_free(bindaddr);
+	m_free(request_addr);
 	TRACE(("leave cancelremotetcp"))
 	return ret;
 }

test/test_canceltcplisten.py
@@ -1,0 +1,51 @@
+"""
+Tests cancel-tcpip-forward
+"""
+from test_dropbear import *
+
+import asyncssh
+import asyncio
+import socket
+import errno
+import random
+
+async def run(addr, port):
+    async with asyncssh.connect(addr, port = port) as conn:
+
+        fwds = []
+        COUNT=4
+
+        def connect(host, port):
+            print(f"connection from {host}:{port}")
+
+        for x in range(COUNT):
+            server = await conn.start_server(connect, '', 0,
+                                     encoding='utf-8')
+            print(f"opened fwd {server} port {server.get_port()}")
+            fwds.append(server)
+
+        # Check that they can be connected
+        for f in fwds:
+            c = socket.create_connection(("localhost", f.get_port()))
+
+        # In case list ordering is important
+        random.shuffle(fwds)
+
+        for f in fwds:
+            print(f"close fwd {f}")
+            f.close()
+            await f.wait_closed()
+
+            # Check it's closed
+            try:
+                c = socket.create_connection(("localhost", f.get_port()))
+                assert False, f"Expected {f} port {f.get_port()} to be closed"
+            except OSError as e:
+                assert e.errno == errno.ECONNREFUSED
+
+def test_canceltcplisten(request, dropbear):
+    opt = request.config.option
+    host = opt.remote or LOCALADDR
+    port = int(opt.port)
+
+    asyncio.run(run(host, port))