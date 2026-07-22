mkj/dropbear

commit 7af125aa4bb8b32060bc0ad14a4e1bd32f165e75

Move ctype replacement functions to dbutil.c
Can avoid the compiler specifics and extra file, and small platforms
should be using LTO these days anyway.

src/dbctype.h
@@ -1,49 +1,0 @@
-#ifndef DROPBEAR_DBCTYPE_H_
-#define DROPBEAR_DBCTYPE_H_
-
-/* Locale-independent replacements for the <ctype.h> character
-   classification functions. The standard ones have undefined behaviour
-   when passed a plain char with a value that doesn't fit in unsigned
-   char, and their results can vary with locale. Dropbear only cares
-   about ASCII ranges, so simple comparisons suffice. Bytes outside
-   0-127 never match. */
-
-/* c89 has no "inline" keyword. gcc and clang accept __inline__ in
-   any mode; otherwise fall back to plain static functions. */
-#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
-#define DBCTYPE_INLINE static inline
-#elif defined(__GNUC__)
-#define DBCTYPE_INLINE static __inline__
-#else
-#define DBCTYPE_INLINE static
-#endif
-
-DBCTYPE_INLINE int ascii_isdigit(char c) {
-	return c >= '0' && c <= '9';
-}
-
-DBCTYPE_INLINE int ascii_isalpha(char c) {
-	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-}
-
-DBCTYPE_INLINE int ascii_isalnum(char c) {
-	return ascii_isalpha(c) || ascii_isdigit(c);
-}
-
-DBCTYPE_INLINE int ascii_isspace(char c) {
-	return c == ' ' || c == '\t' || c == '\n'
-		|| c == '\v' || c == '\f' || c == '\r';
-}
-
-DBCTYPE_INLINE int ascii_isprint(char c) {
-	return c >= 0x20 && c <= 0x7e;
-}
-
-DBCTYPE_INLINE char ascii_tolower(char c) {
-	if (c >= 'A' && c <= 'Z') {
-		return c + ('a' - 'A');
-	}
-	return c;
-}
-
-#endif /* DROPBEAR_DBCTYPE_H_ */

src/dbutil.c
@@ -879,3 +879,38 @@ void cleantext(char* dirtytext, int allow_whitespace) {
 	/* Null terminate */
 	dirtytext[j] = '\0';
 }
+
+/* Locale-independent replacements for the <ctype.h> character
+   classification functions. The standard ones have undefined behaviour
+   when passed a plain char with a value that doesn't fit in unsigned
+   char, and their results can vary with locale. Dropbear only cares
+   about ASCII ranges, so simple comparisons suffice. Bytes outside
+   0-127 never match. */
+
+int ascii_isdigit(char c) {
+	return c >= '0' && c <= '9';
+}
+
+int ascii_isalpha(char c) {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+int ascii_isalnum(char c) {
+	return ascii_isalpha(c) || ascii_isdigit(c);
+}
+
+int ascii_isspace(char c) {
+	return c == ' ' || c == '\t' || c == '\n'
+		|| c == '\v' || c == '\f' || c == '\r';
+}
+
+int ascii_isprint(char c) {
+	return c >= 0x20 && c <= 0x7e;
+}
+
+char ascii_tolower(char c) {
+	if (c >= 'A' && c <= 'Z') {
+		return c + ('a' - 'A');
+	}
+	return c;
+}

src/dbutil.h
@@ -119,4 +119,12 @@ int scp_main(int argc, char ** argv);
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
 
+/* Sensible ctype alternatives */
+int ascii_isdigit(char c);
+int ascii_isalpha(char c);
+int ascii_isalnum(char c);
+int ascii_isspace(char c);
+int ascii_isprint(char c);
+char ascii_tolower(char c);
+
 #endif /* DROPBEAR_DBUTIL_H_ */

src/includes.h
@@ -148,7 +148,6 @@
 #endif
 
 #include "compat.h"
-#include "dbctype.h"
 
 #ifndef HAVE_U_INT8_T
 typedef unsigned char u_int8_t;

commit 5ae1e29a2e86ae567bc3c301555f7341b2d37249

scp: Fix ascii_isdigit build failure
Move ctype replacements to dbctype.c, include it for scp.

Fixes: 7af125aa4bb8 ("Move ctype replacement functions to dbutil.c")

Makefile.in
@@ -44,7 +44,7 @@ _COMMONOBJS=dbutil.o buffer.o dbhelpers.o \
 		atomicio.o compat.o fake-rfc2553.o \
 		ltc_prng.o ecc.o ecdsa.o sk-ecdsa.o crypto_desc.o \
 		curve25519.o ed25519.o sk-ed25519.o \
-		dbmalloc.o \
+		dbmalloc.o dbctype.o \
 		gensignkey.o gendss.o genrsa.o gened25519.o
 COMMONOBJS = $(patsubst %,$(OBJ_DIR)/%,$(_COMMONOBJS))
 
@@ -74,7 +74,7 @@ KEYOBJS = $(patsubst %,$(OBJ_DIR)/%,$(_KEYOBJS))
 _CONVERTOBJS=dropbearconvert.o keyimport.o signkey_ossh.o
 CONVERTOBJS = $(patsubst %,$(OBJ_DIR)/%,$(_CONVERTOBJS))
 
-_SCPOBJS=scp.o progressmeter.o atomicio.o scpmisc.o compat.o
+_SCPOBJS=scp.o progressmeter.o atomicio.o scpmisc.o compat.o dbctype.o
 SCPOBJS = $(patsubst %,$(OBJ_DIR)/%,$(_SCPOBJS))
 
 ifeq (@DROPBEAR_FUZZ@, 1)

src/dbctype.c
@@ -1,0 +1,36 @@
+#include "dbctype.h"
+
+/* Locale-independent replacements for the <ctype.h> character
+   classification functions. The standard ones have undefined behaviour
+   when passed a plain char with a value that doesn't fit in unsigned
+   char, and their results can vary with locale. Dropbear only cares
+   about ASCII ranges, so simple comparisons suffice. Bytes outside
+   0-127 never match. */
+
+int ascii_isdigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+int ascii_isalpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+int ascii_isalnum(char c) {
+    return ascii_isalpha(c) || ascii_isdigit(c);
+}
+
+int ascii_isspace(char c) {
+    return c == ' ' || c == '\t' || c == '\n'
+        || c == '\v' || c == '\f' || c == '\r';
+}
+
+int ascii_isprint(char c) {
+    return c >= 0x20 && c <= 0x7e;
+}
+
+char ascii_tolower(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return c + ('a' - 'A');
+    }
+    return c;
+}

src/dbctype.h
@@ -1,0 +1,13 @@
+#ifndef DBCTYPE_H
+#define DBCTYPE_H
+
+/* Sensible ctype alternatives */
+int ascii_isdigit(char c);
+int ascii_isalpha(char c);
+int ascii_isalnum(char c);
+int ascii_isspace(char c);
+int ascii_isprint(char c);
+char ascii_tolower(char c);
+
+#endif
+

src/dbutil.c
@@ -879,38 +879,3 @@ void cleantext(char* dirtytext, int allow_whitespace) {
 	/* Null terminate */
 	dirtytext[j] = '\0';
 }
-
-/* Locale-independent replacements for the <ctype.h> character
-   classification functions. The standard ones have undefined behaviour
-   when passed a plain char with a value that doesn't fit in unsigned
-   char, and their results can vary with locale. Dropbear only cares
-   about ASCII ranges, so simple comparisons suffice. Bytes outside
-   0-127 never match. */
-
-int ascii_isdigit(char c) {
-	return c >= '0' && c <= '9';
-}
-
-int ascii_isalpha(char c) {
-	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-}
-
-int ascii_isalnum(char c) {
-	return ascii_isalpha(c) || ascii_isdigit(c);
-}
-
-int ascii_isspace(char c) {
-	return c == ' ' || c == '\t' || c == '\n'
-		|| c == '\v' || c == '\f' || c == '\r';
-}
-
-int ascii_isprint(char c) {
-	return c >= 0x20 && c <= 0x7e;
-}
-
-char ascii_tolower(char c) {
-	if (c >= 'A' && c <= 'Z') {
-		return c + ('a' - 'A');
-	}
-	return c;
-}

src/dbutil.h
@@ -118,13 +118,4 @@ int scp_main(int argc, char ** argv);
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
-
-/* Sensible ctype alternatives */
-int ascii_isdigit(char c);
-int ascii_isalpha(char c);
-int ascii_isalnum(char c);
-int ascii_isspace(char c);
-int ascii_isprint(char c);
-char ascii_tolower(char c);
-
 #endif /* DROPBEAR_DBUTIL_H_ */

src/includes.h
@@ -148,6 +148,7 @@
 #endif
 
 #include "compat.h"
+#include "dbctype.h"
 
 #ifndef HAVE_U_INT8_T
 typedef unsigned char u_int8_t;